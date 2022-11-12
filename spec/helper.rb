d37f7dcef83fc2fd269800a4756c04ba1367e826
Mike Blumtritt <mike.blumtritt@pm.me>
2022-11-12 16:37:02 +0100

    Tidy-up spec/helper

--- spec/helper.rb
+++ spec/helper.rb
@@ -3,7 +3,6 @@
 require 'rspec/core'
 require_relative '../lib/parse-argv'
 
-$stdout.sync = $stderr.sync = true
-$VERBOSE = true
-RSpec.configure { |config| config.disable_monkey_patching! }
+$stdout.sync = $stderr.sync = $VERBOSE = true
+RSpec.configure(&:disable_monkey_patching!)
 ParseArgv.on_error(:raise)
